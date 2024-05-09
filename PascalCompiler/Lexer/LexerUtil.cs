72aadfb6fd7e68b4d659cbeb632639bbf7021e20
TheUbMunster <theubmunster@gmail.com>
2024-05-09 05:54:14 -0600

    update readme, trying to determine separation between lexer and parser.

--- PascalCompiler/Lexer/LexerUtil.cs
+++ PascalCompiler/Lexer/LexerUtil.cs
@@ -22,19 +22,22 @@ namespace PascalCompiler.Lexer
       }
       public override string ToString() => ToString(false);
    }
-   public enum TokenType
-   {
+   public enum TokenType : uint
+   { //any commented out entries represent concepts that were likely migrated to the parser.
       //*direct* explicit usage for "digits" and "letters" according to 6.1.1 is unclear, commented if/until needed.
+      //rebuttal for above: https://stackoverflow.com/questions/3192619/design-guidelines-for-parser-and-lexer
+      //character-strings (see 6.1.7)
+      StringCharacter, // [0-9A-Za-z_-\(\)]
+      ApostropheImage, // "
       //digit (see 6.1.1)
-      //_0, _1, _2, _3, _4, _5, _6, _7, _8, _9,
+      _0, _1, _2, _3, _4, _5, _6, _7, _8, _9,
       //letter (see 6.1.1)
-      //A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
-
-
+      A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
+      a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z,
       //identifier (see 6.1.3)
-      Identifier, //one letter followed by one or more letters and/or digits.
+      ///*DYNAMIC*/ Identifier, //one letter followed by zero or more letters and/or digits.
       //directive (see 6.1.4)
-      Directive, //one letter followed by one or more letters and/or digits.
+      ///*DYNAMIC*/ Directive, //one letter followed by zero or more letters and/or digits.
       //word-symbol (see 6.1.2)
       And, // and
       Array, // array
@@ -85,7 +88,7 @@ namespace PascalCompiler.Lexer
       Comma, // ,
       Colon, // :
       Semicolon, // ;
-      UpArrow, // ↑
+      /*DYNAMIC*/ UpArrow, // ↑ *OR* asdf (see x.x.x)
       OpenParen, // (
       CloseParen, // )
       KetPair, // <>
@@ -93,8 +96,21 @@ namespace PascalCompiler.Lexer
       GreaterThanOrEqual, // >=
       Walrus, // :=
       DoubleDot, // ..
+      //numbers (see 6.1.5)
+      //some entries omitted, as elements corresponding to these types are disambiguated and constructed from other tokens in a later step.
+      //namely, any qualifying token can be implicitly upcast to signed-number or unsigned-number
+      ///*DYNAMIC*/ SignedReal, // optionally a sign followed by an unsigned-real
+      ///*DYNAMIC*/ SignedInteger, // optionally a sign followed by an unsigned-integer
+      ///*DYNAMIC*/ Sign, // + *OR* -
+      ///*DYNAMIC*/ UnsignedReal, // a digit-sequence followed by a dot followed by a fractional-part followed optionally by (e followed by a scale-factor), *OR* a digit-sequence followed by e followed by a scale-factor
+      ///*DYNAMIC*/ UnsignedInteger, // a digit-sequence
+      ///*DYNAMIC*/ FractionalPart, // a digit-sequence
+      ///*DYNAMIC*/ ScaleFactor, //optionally a sign followed by a digit-sequence
+      ///*DYNAMIC*/ DigitSequence, // a digit followed by zero or more digits
+      //labels (see 6.1.6)
+      ///*DYNAMIC*/ Label, // a digit sequence
       //not explicit in the spec, but useful for this implementation
-      WHITESPACE, //not including cr or lf
+      WHITESPACE = 65536u, //not including cr or lf
       CR,
       LF
    }
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 **The below information may not be applicable (yet) as this is a WIP**
 
-This is a Pascal compiler I wrote as a personal project in C#, specifically, adhering to ISO/IEC 7185:1990.
+This is a Pascal compiler I wrote as a personal project in C#, specifically, adhering to ISO/IEC 7185:1990(E).
 Any reference to "the spec", refers to this spec.
 It probably has some flaws, so I wouldn't use it for anything important, just a fun project.
 Shooting for ?linux x86_64 ABI?.
@@ -14,3 +14,14 @@ Lexer->Parser->Check->Emit->Assemble (MASM)->Link (ld)
 
 This compiler supports optimization flags (O0, O1, O2, O3). Not that any optimizations are particularly impressive, but simply
 proof that the code structure supports conditional optimization goals.
+
+One of the fun parts about writing compilers is how oftentimes the spec leaves to the developer certain implementation details.
+Besides adhering to ISO/IEC 7185:1990(E), here are the details about various parts of my compiler that are relevant to those who use it.
+
+##string-character
+
+Referenced at the beginning of section 6.1.7 is lexem "string-character". This lexem is a set of characters determined to be legal within a string.
+Although it's contents are up to the compiler developer (me), it does have a restriction, namely, the apostrophe ''' is not a legal string-character.
+For now (subject to change), the following C# style regex matches all characters I've determined to be a member of "string-character".
+
+[0-9A-Za-z_-\(\)]
